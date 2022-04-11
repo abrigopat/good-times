dea9768bd3e5278134dbce23068e50e592d2e716
unknown <pmpabrigo@gmail.com>
2022-04-11 13:07:43 +0800

    footer page

---
+++ stylesheet/footer.php
@@ -0,0 +1,22 @@
+    <!-- footer starts here -->
+    <section class="footer wow fadeIn">
+        <div class="footer-con">
+            <!-- <h1 class="typing row"></h1> -->
+            <h1 class="typing"></h1>
+            <p class="">Hi, Love! <br>
+                Hope you had fun exploring, and reading my what-nots in life.<br>
+                If ever you want to collaborate, create something magical or <br>
+                just simply wants to say hi, hit me up and I'll try my best to get back to you!
+            </p>
+
+            <button class="cta-btn">
+                <a href="contact.php" data-hover="hello">Say Hello</a>
+            </button>
+        </div>
+    </section>
+
+     <!-- wow.js -->
+     <script src="src/js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
