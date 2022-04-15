e47713b7835dea5bac9bbb1944c6ab26f711a878
unknown <pmpabrigo@gmail.com>
2022-04-15 14:51:50 +0800

    navbar section

---
+++ stylesheet/navbar.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;500;600;700&family=PT+Serif:wght@400;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylesheet/navbar.css">
+    <title>Navigation</title>
+</head>
+
+<body>
+    <body style="background-color: #f3a58f;">
+    <header>
+        <nav class="my-navbar navbar navbar-expand-lg navbar-light fixed-top">
+            <div class=" container-fluid">
+                <a class="navbar-brand ms-4 d-none d-sm-none d-md-block" href="./index.html">
+
+                    <img class="logo img-fluid" src="home/images/logo.png">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu"
+                    aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarMenu">
+                    
+                    <ul class="navbar-nav pe-5 ms-auto mb-2 mb-lg-0 text-center">
+                        
+                        <li class="nav-item">
+                            <a class="nav-link fw-bolder" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="social-diaries.php">Social Diaries</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="lokal.php">Lokal Adventure</a>
+                        </li>
+                        <li class="contact d-flex justify-content-evenly container " id="contact">
+                            <a class="nav-link" href="contact.php">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
